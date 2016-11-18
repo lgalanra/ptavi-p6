45491b7c3457f02c8e6e23f720d0f94bae17b302
Luis Galán <luisgalanramil@gmail.com>
2016-11-18 13:55:18 +0100

    punto de partida en cliente mío de p4

--- client.py
+++ client.py
@@ -1,32 +1,35 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Programa cliente que abre un socket a un servidor
+Programa cliente UDP que abre un socket a un servidor
 """
 
 import socket
+import sys
 
-# Cliente UDP simple.
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+if __name__ == "__main__":
+    # Constantes. Dirección IP del servidor y contenido a enviar
+    try:
+        SERVER = sys.argv[1]
+        PORT = int(sys.argv[2])
+        register = sys.argv[3]
+        direction = sys.argv[4]
+        expires_value = int(sys.argv[5])
+    except ValueError:
+        sys.exit("Usage: client.py ip puerto register\
+                 /sip_address expires_value")
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+        my_socket.connect((SERVER, PORT))
+        if sys.argv[3] == 'register':
+            my_socket.send(bytes('REGISTER sip:' + sys.argv[4] +
+                           ' SIP/2.0', 'utf-8') + b'\r\n' +
+                           bytes('Expires: ' + str(expires_value), 'utf-8') +
+                           b'\r\n\r\n')
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+        data = my_socket.recv(1024)
+        print(data.decode('utf-8'))
 
-print("Enviando: " + LINE)
-my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-data = my_socket.recv(1024)
-
-print('Recibido -- ', data.decode('utf-8'))
-print("Terminando socket...")
-
-# Cerramos todo
-my_socket.close()
-print("Fin.")
+        print("Socket terminado.")
