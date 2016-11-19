e8efdf23ad3d488051842168883215803b641c1e
Luis Galán <luisgalanramil@gmail.com>
2016-11-19 12:02:01 +0100

    añadido troceo entrada y mensaje error

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Programa cliente UDP que abre un socket a un servidor
+Programa cliente SIP que abre un socket a un servidor
 """
 
 import socket
@@ -11,18 +11,17 @@ import sys
 if __name__ == "__main__":
     # Constantes. Dirección IP del servidor y contenido a enviar
     try:
-        SERVER = sys.argv[1]
-        PORT = int(sys.argv[2])
-        register = sys.argv[3]
-        direction = sys.argv[4]
-        expires_value = int(sys.argv[5])
+        METHOD = sys.argv[1]
+        destination = str(sys.argv[2])
+        LOGIN = destination[:destination.find('@')]
+        IP = destination[destination.find('@')+1 : destination.find(':')]
+        PORT = int(destination[destination.find(':')+1 :])
     except ValueError:
-        sys.exit("Usage: client.py ip puerto register\
-                 /sip_address expires_value")
+        sys.exit("Usage: client.py method receiver@IP:SIPport")
 
     # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-        my_socket.connect((SERVER, PORT))
+        my_socket.connect((IP, PORT))
         if sys.argv[3] == 'register':
             my_socket.send(bytes('REGISTER sip:' + sys.argv[4] +
                            ' SIP/2.0', 'utf-8') + b'\r\n' +
