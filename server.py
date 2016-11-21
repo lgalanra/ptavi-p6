f7e55db03b47c56f1c9ceb55b36094796be417e6
Luis Galán <luisgalanramil@gmail.com>
2016-11-21 16:34:07 +0100

    arreglar rtp

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ Clase (y programa principal) para un servidor SIP peer2peer
 
 import socketserver
 import sys
+import os
 
 
 class SIPHandler(socketserver.DatagramRequestHandler):
@@ -25,7 +26,10 @@ class SIPHandler(socketserver.DatagramRequestHandler):
             self.wfile.write(b'SIP/2.0 100 Trying\r\n')
             self.wfile.write(b'SIP/2.0 180 Ring\r\nSIP/2.0 200 OK\r\n\r\n')
         elif info.startswith('ACK'):
-            pass #envío RTP
+            #aEjecutar es un string con lo que se ha de ejecutar en la shell
+            aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 <' + fichero_audio
+            print ('Vamos a ejecutar', aEjecutar)
+            os.system(aEjecutar)
         elif info.startswith('BYE'):
             self.wfile.write(b'Finishing...')
         else:
@@ -35,6 +39,7 @@ if __name__ == "__main__":
     try:
         IP = sys.argv[1]
         PORT = int(sys.argv[2])
+        fichero_audio = str(sys.argv[3])
     except ValueError:
         print("Usage: python server.py IP port audio_file")
     serv = socketserver.UDPServer(('', PORT), SIPHandler)
